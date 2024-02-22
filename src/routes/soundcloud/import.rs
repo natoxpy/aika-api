b51d75cf011245eb86adebaf76ac157d9ac17ccb
toxpy <natoxpy@gmail.com>
2024-02-22 07:09:12 -0500

    fix(api): Soundcloud import method not saving data
    - Save method are now awaited
    - Save method are now in the correct order to prevent sql reference constraint errors
    - Music audio is now saved

--- src/routes/soundcloud/import.rs
+++ src/routes/soundcloud/import.rs
@@ -1,12 +1,14 @@
 use actix_web::{post, web, HttpResponse, Responder};
+use sclouddl::track::Track;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::{
     db::{
-        content::Music,
-        content_refs::{MusicArtistRef, MusicImageRef},
+        content::{Audio, Music},
+        content_refs::{MusicArtistRef, MusicAudioRef, MusicImageRef},
     },
+    routes::fs::upload::upload_from_url_standalone,
     states::DB,
 };
 
@@ -17,7 +19,7 @@ pub struct ImportData {
     pub soundcloud_url: String,
     pub artists_id: Vec<Uuid>,
     pub featured_artists_id: Vec<Uuid>,
-    pub album_id: Option<Uuid>,
+    pub albums_id: Vec<Uuid>,
 }
 
 #[post("/import")]
@@ -27,6 +29,29 @@ async fn import(data: web::Json<ImportData>, db: web::Data<DB>) -> impl Responde
         name: data.title.clone(),
     };
 
+    db.tables.musics().save(music.clone()).await;
+
+    let track = Track::get_song(&data.soundcloud_url).await;
+
+    let media = track.media.get_progressive(&track.client_id).await;
+    let audio_file = upload_from_url_standalone(media).await;
+
+    let audio_record = Audio {
+        id: Uuid::new_v4(),
+        file: audio_file.id,
+    };
+
+    let music_audio_ref = MusicAudioRef {
+        id: Uuid::new_v4(),
+        music: music.id,
+        audio: audio_record.id,
+    };
+
+    db.tables.files().save(audio_file).await;
+    db.tables.audios().save(audio_record).await;
+
+    db.tables.refs().music_audio().save(music_audio_ref).await;
+
     for artist in data.artists_id.iter() {
         let music_artist = MusicArtistRef {
             id: Uuid::new_v4(),
@@ -34,7 +59,7 @@ async fn import(data: web::Json<ImportData>, db: web::Data<DB>) -> impl Responde
             artist: artist.clone(),
         };
 
-        db.tables.refs().music_artist().save(music_artist);
+        db.tables.refs().music_artist().save(music_artist).await;
     }
 
     for featured_artist in data.featured_artists_id.iter() {
@@ -44,7 +69,7 @@ async fn import(data: web::Json<ImportData>, db: web::Data<DB>) -> impl Responde
             artist: featured_artist.clone(),
         };
 
-        db.tables.refs().music_artist().save(music_artist);
+        db.tables.refs().music_artist().save(music_artist).await;
     }
 
     let music_cover = MusicImageRef {
@@ -53,8 +78,7 @@ async fn import(data: web::Json<ImportData>, db: web::Data<DB>) -> impl Responde
         image: data.image_id,
     };
 
-    db.tables.musics().save(music.clone());
-    db.tables.refs().music_image().save(music_cover);
+    db.tables.refs().music_image().save(music_cover).await;
 
     HttpResponse::Ok().json(music)
 }
