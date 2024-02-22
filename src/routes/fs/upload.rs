2ae71ef9524282916772fc73818d1d8b6cd2f5b1
toxpy <natoxpy@gmail.com>
2024-02-22 07:14:05 -0500

    refactor(api): Filesystem upload standalone method
    - fs upload route functionality was turned into a standalone method to reuse elsewhere

--- src/routes/fs/upload.rs
+++ src/routes/fs/upload.rs
@@ -1,13 +1,12 @@
-use crate::db::content;
+use crate::db::content::{self, File};
 use actix_web::{post, web, HttpResponse, Responder};
 use std::{fs, io::Write, path::PathBuf};
 use uuid::Uuid;
 
 use crate::states::{DB, FILES};
 
-#[post("/upload/{file_url}")]
-async fn upload_from_url(db: web::Data<DB>, file_url: web::Path<String>) -> impl Responder {
-    let response = reqwest::get(file_url.into_inner()).await.unwrap();
+pub async fn upload_from_url_standalone(file_url: String) -> File {
+    let response = reqwest::get(file_url).await.unwrap();
     let body = response.bytes().await.unwrap();
     let mime = infer::get(&body).unwrap();
     let id = Uuid::new_v4();
@@ -20,13 +19,18 @@ async fn upload_from_url(db: web::Data<DB>, file_url: web::Path<String>) -> impl
 
     file_fs.write_all(&body).unwrap();
 
-    let file_record = content::File {
+    content::File {
         id,
         name: file_name.to_string(),
         location: format!("{}{}", location, file_name),
         mime: mime.to_string(),
         size: body.len() as u64,
-    };
+    }
+}
+
+#[post("/upload/{file_url}")]
+async fn upload_from_url(db: web::Data<DB>, file_url: web::Path<String>) -> impl Responder {
+    let file_record = upload_from_url_standalone(file_url.into_inner()).await;
 
     db.tables.files().save(file_record.clone()).await;
 
